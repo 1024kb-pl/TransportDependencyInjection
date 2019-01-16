a481ef02bbdb4ced5f391a67f96f39d6a8d813e6
klimson <klimson@gitlab.com>
2019-01-16 19:25:36 +0100

    Model changes

--- src/org/blog/Main.java
+++ src/org/blog/Main.java
@@ -5,7 +5,7 @@ import org.blog.transport.TransportTypes;
 public class Main {
 
     public static void main(String[] args) {
-        DeliveryPackage deliveryPackage = new DeliveryPackage("Książka", "Effective Java");
+        DeliveryPackage deliveryPackage = new DeliveryPackage("Książka", "Effective Java", "Księżyć 103");
 
         DeliveryService deliveryService = new DeliveryService();
         deliveryService.sendPackage(deliveryPackage, TransportTypes.AIRPLANE);
