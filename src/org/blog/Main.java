5fe89f06de70fdba9e80ba8d60f6d9a1415d26f5
klimson <klimson@gitlab.com>
2019-01-16 19:28:32 +0100

    Model changes

--- src/org/blog/Main.java
+++ src/org/blog/Main.java
@@ -5,7 +5,7 @@ import org.blog.transport.AirplaneTransport;
 public class Main {
 
     public static void main(String[] args) {
-        DeliveryPackage deliveryPackage = new DeliveryPackage("Książka", "Effective Java");
+        DeliveryPackage deliveryPackage = new DeliveryPackage("Książka", "Effective Java", "Ksiezyc 103");
 
         DeliveryService deliveryService = new DeliveryService(new AirplaneTransport());
         deliveryService.sendPackage(deliveryPackage);
